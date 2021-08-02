ccb1e5a3f1318a83ea8db2fc5a22e24c5a1360dc
vutran1305 <tranlongvu_t63@hus.edu.vn>
2021-08-02 15:27:21 +0700

    update v1

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # SECRET_KEY = os.environ.get('SECRET_KEY') or 'khong-doan-noi-dau' # khóa mã hóa để tạo ra  các token bảo mật
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://localhost/[YOUR_DATABASE_NAME]'
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     
